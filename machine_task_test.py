818ade5da39e4b496c7a563f5c82fc34d5919398
Gautier Dagan <gautierdagan2017@u.northwestern.edu>
2018-11-15 17:11:37 +0100

    TST: Added task test file
    TST: Added task test file that simply gets the task object according to the name and prints them. This should offer an example of how to use the Task interface

---
+++ machine_task_test.py
@@ -0,0 +1,10 @@
+from tasks.tasks import get_task
+
+
+task_a = get_task("long_lookup_reverse")
+task_b = get_task("lookup")
+task_c = get_task("symbol_rewriting")
+
+print(task_a)
+print(task_b)
+print(task_c)
